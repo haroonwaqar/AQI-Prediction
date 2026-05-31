7b2d5f7576d7bb3263be05f13006f75ea2138321
Haroon Waqar <haroonwaqar1234@gmail.com>
2026-05-31 23:13:29 +0500

    model training

---
+++ training_pipline.py
@@ -0,0 +1,94 @@
+import os
+import hopsworks
+import pandas as pd
+import joblib
+from dotenv import load_dotenv
+
+# Machine Learning Libraries
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Ridge
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+from sklearn.metrics import root_mean_squared_error, mean_absolute_error, r2_score
+#from sklearn.metrics import accuracy_score, confusion_matrix, f1_score
+
+# 1. Load your API key
+load_dotenv()
+api_key = os.getenv("HopsworkAPI_KEY")
+
+project = hopsworks.login(api_key_value=api_key)
+
+fs = project.get_feature_store()
+
+fg = fs.get_feature_group("sialkot_aqi_features", version=1)
+df = fg.read()
+
+# Dropping features not needed
+# We drop columns that leak the answer, or that math can't understand
+columns_to_drop = ['city', 'date', 'datetime', 'aqi', 'pm2_5', 'pm10']
+
+X = df.drop(columns=columns_to_drop)
+
+y = df['pm2_5']
+
+# Use train_test_split to hide 20% of the data. Set random_state=42 for reproducibility.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train the models
+# Initialize the competitors
+models = {
+    "Ridge_Regression": Ridge(),
+    "Random_Forest": RandomForestRegressor(n_estimators=100, random_state=42),
+    "XGBoost": XGBRegressor(n_estimators=100, random_state=42)
+}
+
+# Dictionary to keep track of the results
+results = {}
+
+# Train and evaluate each model in a loop
+for name, model in models.items():
+    # Train the model using the .fit() method on your training data
+    model.fit(X_train, y_train)
+    
+    # Ask the model to predict PM2.5 for the hidden X_test data
+    predictions = model.predict(X_test)
+    
+    # Calculate the three required metrics by comparing 'predictions' to 'y_test'
+    rmse = root_mean_squared_error(y_test, predictions) 
+    mae = mean_absolute_error(y_test, predictions)
+    r2 = r2_score(y_test, predictions)
+    
+    results[name] = {"RMSE": rmse, "MAE": mae, "R2": r2, "model_object": model}
+    print(f"{name} trained. R2 Score: {r2:.3f}")
+
+
+# Find the model with the highest R2 score
+best_model_name = max(results, key=lambda k: results[k]['R2'])
+best_metrics = results[best_model_name]
+champion_model = best_metrics['model_object']
+
+print(f"\nWINNER: {best_model_name} with an R2 of {best_metrics['R2']:.3f}")
+
+
+# Push the best model to hopswork
+# Save the model locally first as a .pkl file
+model_dir = "aqi_model_dir"
+os.makedirs(model_dir, exist_ok=True)
+joblib.dump(champion_model, f"{model_dir}/sialkot_pm25_model.pkl")
+
+# Connect to the Model Registry
+mr = project.get_model_registry()
+
+# Create the cloud model entity, attaching our specific metrics
+sialkot_model = mr.python.create_model(
+    name="sialkot_pm2_5_predictor",
+    metrics={
+        "RMSE": best_metrics['RMSE'],
+        "MAE": best_metrics['MAE'],
+        "R2": best_metrics['R2']
+    },
+    description=f"Predicts PM2.5 using algorithm: {best_model_name}"
+)
+
+# Upload the local directory to the cloud
+sialkot_model.save(model_dir)
