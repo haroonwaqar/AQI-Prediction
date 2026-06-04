efcff3c95a9189388e469f1e52bf202042c0051a
Haroon Waqar <haroonwaqar1234@gmail.com>
2026-06-05 00:39:44 +0500

    new features

--- training_pipline.py
+++ training_pipline.py
@@ -2,11 +2,10 @@ import os
 import hopsworks
 import pandas as pd
 import joblib
+import numpy as np
 from dotenv import load_dotenv
-
-# Machine Learning Libraries
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
 from sklearn.metrics import root_mean_squared_error, mean_absolute_error, r2_score
 
 # 1. Load your API key
@@ -20,6 +19,26 @@ fs = project.get_feature_store()
 fg = fs.get_feature_group("sialkot_aqi_features", version=1)
 df = fg.read()
 
+# Create new features from existing ones
+# Have access to future weather via Open-Meteo!
+df['temp_lag1'] = df['apparent_temperature'].shift(1)
+df['wind_rolling_6'] = df['wind_speed_100m'].rolling(6).mean()
+
+df['day_of_week'] = df['datetime'].dt.dayofweek
+df['month']       = df['datetime'].dt.month
+df['is_weekend']  = df['day_of_week'].isin([5, 6]).astype(int)
+
+# Convert the 24-hour clock into a mathematical circle using Sine and Cosine
+df['hour_sin'] = np.sin(2 * np.pi * df['hour'] / 24)
+df['hour_cos'] = np.cos(2 * np.pi * df['hour'] / 24)
+
+# Now safely drop the raw 'hour' column
+df = df.drop(columns=['hour'])
+
+df = df.dropna().reset_index(drop=True)
+
+df = df.sort_values('datetime').reset_index(drop=True)
+
 # Dropping features not needed
 # We drop columns that leak the answer, or that math can't understand
 columns_to_drop = ['city', 'date', 'datetime', 'aqi', 'pm2_5', 'pm10']
@@ -28,12 +47,12 @@ X = df.drop(columns=columns_to_drop)
 
 y = df['pm2_5']
 
-# Use train_test_split to hide 20% of the data. Set random_state=42 for reproducibility.
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Use train_test_split to hide 20% of the data.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
 
-# Train the models
-# Initialize the competitors
-model = RandomForestRegressor(n_estimators=100, random_state=42)
+# Train the model
+# Initialize the model
+model = XGBRegressor(learning_rate=0.1, max_depth=6, n_estimators=200, random_state=42)
 
 model.fit(X_train, y_train)
     
@@ -59,13 +78,13 @@ mr = project.get_model_registry()
 
 # Create the cloud model entity, attaching our specific metrics
 sialkot_model = mr.python.create_model(
-    name="sialkot_pm2_5_predictor",
+    name="sialkot_pm2_5_predictor_v2",
     metrics={
         "RMSE": rmse,
         "MAE": mae,
         "R2": r2
     },
-    description="Predicts PM2.5 using Random Forest Regressor"
+    description="Predicts PM2.5 using Xgboost Regressor"
 )
 
 # Upload the local directory to the cloud
